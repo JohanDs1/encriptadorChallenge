35ccf5c00332863c0d17bfb2930eb7966aa3bae8
Johan <johan.hark19@gmail.com>
2024-06-20 00:13:52 -0500

    prueba estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    './src/**/*.{html,js,jsx,ts,tsx}', // Añade aquí las rutas correctas según tu estructura de proyecto
+    './src/**/*.{html,js,jsx,ts,tsx}', // Asegúrate de que las rutas coincidan con la estructura de tu proyecto
+    './public/index.html',
   ],
   theme: {
-    extend: {
-     
-    },
+    extend: {},
   },
+  plugins: [],
 };
